1c944ee7b9392df4ba9bdca43853d352efe7c381
RobFinch <5563401+robfinch@users.noreply.github.com>
2023-12-28 08:21:07 -0500

    Qupls - update
    - cc64 compiler
    - boot rom

---
+++ software/boot/bootrom.c
@@ -0,0 +1,37 @@
+
+/* Display blinking LEDs while delaying to show CPU is working.
+*/
+private void Delay3s(void)
+{
+	__int64* leds = 0xFEDFFF00;
+	int cnt;
+	
+	for (cnt = 0; cnt < 300000; cnt++)
+		leds[0] = cnt >> 17;
+}
+
+public void bootrom(void)
+{
+	int* PTBR = 0xfff4ff20;
+	int* pgtbl = 0xfff80000;
+	int cnt;
+	__int32* pRand;
+
+	*PTBR = 0xfff80000;	
+	pRand = 0xFEE1FD00;
+	
+	__sync(0x0FFFF);
+	/* clear out page table */
+	for (cnt = 0; cnt < 16; cnt++) {
+		pgtbl[cnt] = 0;
+	}
+	pgtbl[0x1EDF] = 0x83000FFFFFFFFEDF;	/* LEDs */
+	pgtbl[0x1EC0] = 0x83000FFFFFFFFEC0;	/* text mode screen */
+	pgtbl[0x1EDC] = 0x83000FFFFFFFFEDC;	/* Keyboard */
+	pgtbl[0x1EE1] = 0x83000FFFFFFFFEE1;	/* random number generator */
+	__sync(0x0FFFF);
+	pRand[1] = 0;						/* select random stream #0 */
+	pRand[2] = 0x99999999;	/* set random seed value */
+	pRand[3] = 0x99999999;
+	Delay3s();
+}
