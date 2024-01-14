8f85c10183d56f9b4e90448cb85427ad0a8fd753
RobFinch <5563401+robfinch@users.noreply.github.com>
2024-01-14 00:30:19 -0500

    Qupls - update

--- software/util/QuplsUtil/Form1.Designer.cs
+++ software/util/QuplsUtil/Form1.Designer.cs
@@ -31,13 +31,17 @@
 			this.button1 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(555, 37);
+			this.button1.Location = new System.Drawing.Point(554, 37);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(112, 34);
+			this.button1.Size = new System.Drawing.Size(111, 33);
 			this.button1.TabIndex = 0;
 			this.button1.Text = "pusha";
 			this.button1.UseVisualStyleBackColor = true;
@@ -57,19 +61,61 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(553, 94);
+			this.button2.Location = new System.Drawing.Point(553, 93);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(112, 34);
+			this.button2.Size = new System.Drawing.Size(111, 33);
 			this.button2.TabIndex = 2;
 			this.button2.Text = "popa";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.button2_Click);
 			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(554, 153);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(111, 33);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "stctx";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(554, 210);
+			this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(107, 38);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "ldctx";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(746, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(87, 25);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "word size";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(848, 39);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(83, 31);
+			this.textBox2.TabIndex = 6;
+			this.textBox2.Text = "16";
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1434, 886);
+			this.ClientSize = new System.Drawing.Size(1434, 887);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.button1);
@@ -85,5 +131,9 @@
 		private Button button1;
 		private TextBox textBox1;
 		private Button button2;
+		private Button button3;
+		private Button button4;
+		private Label label1;
+		private TextBox textBox2;
 	}
 }
--- software/util/QuplsUtil/Form1.cs
+++ software/util/QuplsUtil/Form1.cs
@@ -1,3 +1,6 @@
+using System.Text.RegularExpressions;
+using System;
+
 namespace QuplsUtil
 {
 	public partial class Form1 : Form
@@ -11,6 +14,7 @@ namespace QuplsUtil
 		{
 			string str;
 			int rg, adr;
+			int size = Int32.Parse(textBox2.Text);
 
 			for (rg = 0; rg < 64; rg++)
 			{
@@ -42,8 +46,17 @@ namespace QuplsUtil
 				}
 				else
 				{
-					adr = (rg - 1) * 8;
-					str = str + "\t\tinstr.ins={21'h" + adr.ToString("X5") + ",SP,6'd" + rg.ToString() + ",OP_STO};\r\n";
+					adr = (rg - 1) * size;
+					str = str + "\t\tinstr.ins={21'h" + adr.ToString("X5") + ",SP,6'd" + rg.ToString();
+					if (size==8)
+					{
+						str = str + ",OP_STO};\r\n";
+					}
+					else if (size==16)
+					{
+						str = str + ",OP_STH};\r\n";
+					}
+
 				}
 				str = str + "\t\tinstr.pred_btst=6'd0;\r\n";
 				str = str + "\tend\r\n";
@@ -55,6 +68,7 @@ namespace QuplsUtil
 		{
 			string str;
 			int rg, rg1, adr;
+			int size = Int32.Parse(textBox2.Text);
 
 			for (rg = 0; rg < 64; rg++)
 			{
@@ -87,14 +101,115 @@ namespace QuplsUtil
 				else
 				{
 					rg1 = rg + 1;
-					adr = rg * 8;
+					adr = rg * size;
 					str = str + "\t\tinstr.aRt=9'd" + rg1.ToString() + ";\r\n";
-					str = str + "\t\tinstr.ins={21'h" + adr.ToString("X5") + ",SP,6'd" + rg1.ToString() + ",OP_LDO};\r\n";
+					str = str + "\t\tinstr.ins={21'h" + adr.ToString("X5") + ",SP,6'd" + rg1.ToString();
+					if (size == 8)
+					{
+						str = str + ",OP_LDO};\r\n";
+					}
+					else if (size == 16)
+					{
+						str = str + ",OP_LDH};\r\n";
+					}
 				}
 				str = str + "\t\tinstr.pred_btst=6'd0;\r\n";
 				str = str + "\tend\r\n";
 				textBox1.AppendText(str);
 			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			string str;
+			int rg, adr;
+			int size = Int32.Parse(textBox2.Text);
+
+			for (rg = 0; rg < 64; rg++)
+			{
+				adr = rg + 0x100;
+				str = "12'h" + adr.ToString("X3");
+				str = str + ":\r\n";
+				str = str + "\tbegin\r\n";
+				adr = (0x100 + (rg + 4)) & 0xffc;
+				str = str + "\t\tnext_ip=12'h" + adr.ToString("X3") + ";\r\n";
+				if (rg == 0)
+				{
+					str = str + "\t\tinstr.aRa=9'd0;\r\n";
+					str = str + "\t\tinstr.aRb=9'd0;\r\n";
+					str = str + "\t\tinstr.aRc=9'd0;\r\n";
+					str = str + "\t\tinstr.aRt=9'd64+MC0;\r\n";
+					str = str + "\t\tinstr.ins = {3'd0,2'd0,CSR_CTX,6'h00,MC0,OP_CSR};\r\n";
+				}
+				else
+				{
+					str = str + "\t\tinstr.aRa=9'd64+MC0;\r\n";
+					str = str + "\t\tinstr.aRb=9'd0;\r\n";
+					str = str + "\t\tinstr.aRc=9'd" + rg.ToString() + ";\r\n";
+					str = str + "\t\tinstr.aRt=9'd0;\r\n";
+					adr = rg * size;
+					str = str + "\t\tinstr.ins={21'h" + adr.ToString("X5") + ",MC0,6'd" + rg.ToString();
+					if (size == 8)
+					{
+						str = str + ",OP_STO};\r\n";
+					}
+					else if (size == 16)
+					{
+						str = str + ",OP_STH};\r\n";
+					}
+				}
+				str = str + "\t\tinstr.pred_btst=6'd0;\r\n";
+				str = str + "\tend\r\n";
+				textBox1.AppendText(str);
+			}
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			string str;
+			int rg, adr;
+			int size = Int32.Parse(textBox2.Text);
+
+			for (rg = 0; rg < 64; rg++)
+			{
+				adr = rg + 0x150;
+				str = "12'h" + adr.ToString("X3");
+				str = str + ":\r\n";
+				str = str + "\tbegin\r\n";
+				adr = (0x150 + (rg + 4)) & 0xffc;
+				str = str + "\t\tnext_ip=12'h" + adr.ToString("X3") + ";\r\n";
+				if (rg == 0)
+				{
+					str = str + "\t\tinstr.aRa=9'd0;\r\n";
+					str = str + "\t\tinstr.aRb=9'd0;\r\n";
+					str = str + "\t\tinstr.aRc=9'd0;\r\n";
+					str = str + "\t\tinstr.aRt=9'd64+MC0;\r\n";
+					str = str + "\t\tinstr.ins = {3'd0,2'd0,CSR_CTX,6'h00,MC0,OP_CSR};\r\n";
+				}
+				else
+				{
+					str = str + "\t\tinstr.aRa=9'd64+MC0;\r\n";
+					str = str + "\t\tinstr.aRb=9'd0;\r\n";
+					str = str + "\t\tinstr.aRc=9'd0;\r\n";
+					str = str + "\t\tinstr.aRt=9'd" + rg.ToString() + ";\r\n";
+					adr = rg * size;
+					str = str + "\t\tinstr.ins={21'h" + adr.ToString("X5") + ",MC0,6'd" + rg.ToString();
+					if (size == 8)
+					{
+						str = str + ",OP_LDO};\r\n";
+					}
+					else if (size == 16)
+					{
+						str = str + ",OP_LDH};\r\n";
+					}
+				}
+				str = str + "\t\tinstr.pred_btst=6'd0;\r\n";
+				str = str + "\tend\r\n";
+				textBox1.AppendText(str);
+			}
+
+
 		}
 	}
 }
